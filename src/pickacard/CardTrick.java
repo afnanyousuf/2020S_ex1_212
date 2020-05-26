b6053601039c5f51870ea2da16e404ffa56a3e8a
Steven2242 <steven.tedesco@gmail.com>
2020-05-26 14:14:07 -0400

    added part-time student

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -6,22 +6,53 @@ package pickacard;
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Steven Tedesco - May 26th, 2020
  */
+
+import java.util.Scanner;
 public class CardTrick {
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
         
         Card[] magicHand = new Card[7];
+        
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
+
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random()*13) + 1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+
+            magicHand[i] = c;
         }
 
+        //for(Card c:magicHand){
+        //    System.out.printf("Suit: %s - Value: %d%n", c.getSuit(), c.getValue());
+        //}
+
         //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Enter your card value(1-13): ");
+        Card userCard = new Card();
+        userCard.setValue(in.nextInt());
+        System.out.print("Enter your card suit (Hearts = 1, Diamonds = 2, Spades = 3, Clubs = 4): ");
+        userCard.setSuit(Card.SUITS[in.nextInt()-1]);
+        
         // and search magicHand here
         //Then report the result here
+
+        for(int i = 0; i < magicHand.length; i++){
+            if(userCard.getValue() == magicHand[i].getValue()
+                && userCard.getSuit().equals(magicHand[i].getSuit())){
+                System.out.println("Your card is card number " + (i+1));
+                break;
+            }else if(i == 6){
+                System.out.println("No Match!");
+            }
+        }
     }
 
 }
