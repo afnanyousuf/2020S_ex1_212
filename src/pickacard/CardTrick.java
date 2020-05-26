de6eda3b2ab5241f94320d852d0b53d20cd0a327
MeguMan <50627838+MeguManDotA@users.noreply.github.com>
2020-05-26 14:45:02 -0400

    Updated Search
    Searches for Lucky Card

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -24,12 +24,18 @@ public class CardTrick {
             System.out.printf("%d %s \n", c.getValue(), c.getSuit());
         }
 
-        Scanner in =new Scanner(System.in);
+        /*Scanner in =new Scanner(System.in);
         System.out.println("Enter a suit (clubs, spades, hearts, diamonds)");
         String suit = in.nextLine();
         suit = (suit.substring(0, 1).toUpperCase()+suit.substring(1).toLowerCase());
         System.out.println("Enter a Number (1-13)");
-        int value = in.nextInt();
+        int value = in.nextInt();*/
+        
+        Card luckyCard = newCard();
+        luckyCard.setValue(7);
+        luckyCard.setSuit(Card.SUITS[1])
+        String suit = luckyCard.getSuit();
+        int value = luckyCard.getValue();
         
         for (int j = 0; j < magicHand.length; j++){
             if(suit.equals(magicHand[j].getSuit())){
