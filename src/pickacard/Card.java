526812a970f552712f564f517c1c18932856982d
pereyram <pereyram@sheridancollege.ca>
2020-05-27 15:49:51 -0400

    Created MARIA_PEREYRA_Ex1 branch and introduced changes in steps 3 & 4 of the exercise in it.

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,21 +1,30 @@
 package pickacard;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack -11, Queen =12, King = 13) A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers. This code is to be used in Exercise
+ * 1. When you create your own branch, add your name as an author.
+ *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Maria Pereyra (May 26, 2020)
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades",
+        "Clubs"};
+
+    /**
+     * Constructs a Card Object with default suit and value.
+     */
+    public Card() {
+        // Intentionally left blank
+    }
+    
     /**
      * @return the suit
      */
@@ -24,6 +33,8 @@ public class Card {
     }
 
     /**
+     * Places a suit for this card.
+     *
      * @param suit the suit to set
      */
     public void setSuit(String suit) {
@@ -31,19 +42,21 @@ public class Card {
     }
 
     /**
-     * @return the value
+     * Retrieves the value of this Card.
+     *
+     * @return the value of the Card
      */
     public int getValue() {
         return value;
     }
 
     /**
+     * Places a value for this Card.
+     *
      * @param value the value to set
      */
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
-    
+
 }
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,91 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Maria Pereyra (May 26, 2020)
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-        
+
+        // Create an array of Card Objects to hold a hand of seven cards.
         Card[] magicHand = new Card[7];
 
+        /*
+        Create seven Card Objects and assign random suit and value to each one.
+        Then, store each object in the magic hand.
+         */
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(randomValue());
+            c.setSuit(Card.SUITS[randomSuit()]);
+            //For debugging
+            /*
+            System.out.println("Card: " + i + "Suit: " + c.getSuit() + 
+                    " value: " + c.getValue());
+            */
+            magicHand[i] = c;
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        // Create Scanner Object to get input from user.
+        Scanner input = new Scanner(System.in);
+
+        /*
+        Prompt the user to choose a card. Set suit and value for the user's 
+        card based on user's input. 
+         */
+        System.out.println("Pick a card, any card!\n"
+                + "First pick a suit (Hearts, Diamonds, Spades, Clubs): ");
+        String userSuit = input.nextLine();
+
+        System.out.println("Now, choose a value for the card "
+                + "(whole number between 1 and 13): ");
+        int userValue = Integer.parseInt(input.nextLine());
+
+        // Compare the user's card with the hand and decide if there is a match.
+        boolean match = false;
+        for (Card card : magicHand) {
+            if (userSuit.equalsIgnoreCase(card.getSuit())
+                    && userValue == card.getValue()) {
+                match = true;
+            }
+        }
+
+        // Display the result of the trick
+        String result = match ? "Here is your card!" : "Sorry, your card is not"
+                + " in the hand.";
+        System.out.println(result);
+
+    }
+
+    /**
+     * Generates a random number between 0 and 3. This method is used to assign
+     * the suit of the card.
+     *
+     * @return a random number between 0 and 3
+     */
+    public static int randomSuit() {
+        int out = (int) (Math.random() * 4);
+        return out;
+    }
+
+    /**
+     * Generates a random number between 1 and 13. This method is used to assign
+     * a value to the card.
+     *
+     * @return a value between 1 and 13
+     */
+    public static int randomValue() {
+        int out = (int) (Math.random() * 13 + 1);
+        return out;
+
     }
 
 }
