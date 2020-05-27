8f2df93ebe3368b3e087008f6b2e07256faeab0d
kushch-s <kushch@sheridancollege.ca>
2020-05-27 16:29:49 -0400

    Added parts of the code needed in the instructions

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,7 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -15,13 +17,42 @@ public class CardTrick {
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            //assigns value from 1 to 13
+            c.setValue((int) (Math.random() * 13 + 1));
+            
+            //assigns value from 0-3
+            c.setSuit(Card.SUITS[((int) (Math.random() * 4))]);
+           
+            //puts Card c into magicHand array
+            magicHand[i] = c;
         }
+        
+        Scanner input = new Scanner(System.in);
+        Card luckyCard = new Card ();
+        
+        System.out.println("Please enter the value of the card from 1 to 13");
+        luckyCard.setValue(input.nextInt());
+        
+        System.out.println("Enter your card suit (Hearts = 1, Diamonds = 2, Spades = 3, Clubs = 4): ");
+        luckyCard.setSuit(Card.SUITS[input.nextInt()-1]);
+        
+        
+       
+        boolean match = false;
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        //checks if the user's card matches the one of the cards in the array
+          for (int i = 0; i < magicHand.length; i++) {
+            if (luckyCard.getValue() == magicHand[i].getValue() && luckyCard.getSuit().equals(magicHand[i].getSuit())) {
+                System.out.println("Card was found in the magic hand!");
+                match = true;
+                break;
+            }
+        }
+        
+        if (!match) {
+            System.out.println("Card was not found in the magic hand!");
+        }
     }
 
 }
